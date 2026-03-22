fff0c10cfd16a2e9f679a9ddba295f717e027445
Harish Puri <harishpuri.in@gmail.com>
2026-03-22 13:14:18 +0530

    Reversed Array, Optimal Solution 0(N)

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,18 +1,38 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ReverseArray {
     public static void main(String[] args) {
-        int[] numbers = new int[] { 1, 4, 3, 2, 6, 5 };
-        System.out.println(firstMethod(numbers));
+        int[] numbers = new int[] { 4, 5, 1, 2 };
+        System.out.println(Arrays.toString(secondMethod(numbers)));
     }
 
-    static ArrayList<Integer> firstMethod(int[] arr){
+    static ArrayList<Integer> firstMethod(int[] arr) {
         ArrayList<Integer> reversedArray = new ArrayList<Integer>();
-        
-        for(int i =arr.length - 1; i >= 0; i--){
+
+        for (int i = arr.length - 1; i >= 0; i--) {
             int element = arr[i];
             reversedArray.add(element);
         }
         return reversedArray;
+    }
+
+    static int[] secondMethod(int[] arr) {
+
+        if(arr == null || arr.length < 2) return arr;
+
+        int leftPointer = 0, rightPointer = arr.length - 1;
+
+        while(leftPointer < rightPointer){
+            int swappMaterial = arr[leftPointer];
+
+            arr[leftPointer] = arr[rightPointer];
+            arr[rightPointer] = swappMaterial;
+
+            leftPointer += 1;
+            rightPointer -= 1;
+        }
+
+        return arr;
     }
 }
