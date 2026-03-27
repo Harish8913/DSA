d16cb863fe54759c85675bc24e64b5a35fc6b9cc
Harish Puri <harishpuri.in@gmail.com>
2026-03-27 21:52:51 +0530

    Sliding Window and MAX SUBARRAY SUM

---
+++ MaxSubArraySum.java
@@ -0,0 +1,24 @@
+public class MaxSubArraySum {
+    public static void main(String[] args) {
+        int[] arr = { 1,1,2,2,2,10,10,10,20,2,2,2,4 };
+        int k = 4;
+
+        int value = test(arr, k);
+        System.out.println(value);
+    }
+
+    public static int test(int[] arr, int k) {
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+
+        int maxSum = windowSum;
+        for (int i = k; i < arr.length; i++) {
+            windowSum = maxSum - arr[i - k] + arr[i];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return maxSum;
+    }
+}
