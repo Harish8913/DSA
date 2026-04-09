d1a65c057d21b65688d8d1a40707307ff3110be6
Harish Puri <harishpuri.in@gmail.com>
2026-04-09 10:19:47 +0530

    LongestSubstringWithoutRepeating O(n)

--- Sliding [ ]/LongestRepeatingString.java
+++ Sliding [ ]/LongestRepeatingString.java
@@ -2,30 +2,23 @@ import java.util.HashMap;
 
 public class LongestRepeatingString {
     public static void main(String[] args) {
-        String s = "pwwekw";
-        lengthOfLongestSubstring(s);
+        String s = "pwwkew";
+        System.out.println(lengthOfLongestSubstring(s));
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        int l = 0,r = 0,maxLength = 0;
-        HashMap<Character, Integer> alpha = new HashMap<>();
-        for(char c = 'a'; c < 'z'; c++){alpha.put(c, 0);}
+        int l = 0, maxLength = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
 
-        while(r < s.length()){
-            if(alpha.get(s.charAt(r)) == 0){
-                alpha.put(s.charAt(r), 1);
-                r++;
-            }else{
-                while(alpha.get(s.charAt(l)) == 1){
-                    alpha.put(s.charAt(l), 0);
-                    l++;
-                }
+        for(int r = 0; r < s.length(); r++){
+            char c = s.charAt(r);
+            if(map.containsKey(c)){
+                l = Math.max(l, map.get(c) + 1);
             }
 
+            map.put(c, r);
             maxLength = Math.max(maxLength, r - l + 1);
         }
-
-        System.out.println(maxLength);
 
         return maxLength;
     }
