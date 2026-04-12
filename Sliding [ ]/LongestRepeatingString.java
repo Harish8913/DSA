136b1a3729468ffef8569a375189d4bd747e051c
Harish Puri <harishpuri.in@gmail.com>
2026-04-12 15:29:57 +0530

    MaxSubarray O(n)

--- Sliding [ ]/LongestRepeatingString.java
+++ Sliding [ ]/LongestRepeatingString.java
@@ -2,22 +2,24 @@ import java.util.HashMap;
 
 public class LongestRepeatingString {
     public static void main(String[] args) {
-        String s = "pwwkew";
+        String s = "abcabcbabcabcababaa";
         System.out.println(lengthOfLongestSubstring(s));
     }
 
     public static int lengthOfLongestSubstring(String s) {
         int l = 0, maxLength = 0;
         HashMap<Character, Integer> map = new HashMap<>();
-
-        for(int r = 0; r < s.length(); r++){
-            char c = s.charAt(r);
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
             if(map.containsKey(c)){
+                System.out.println("Current r : " + i);
+                System.out.println(l + " " + (map.get(c) + 1) + " Before Change");
                 l = Math.max(l, map.get(c) + 1);
+                System.out.println(l + " " + " After Change");
             }
 
-            map.put(c, r);
-            maxLength = Math.max(maxLength, r - l + 1);
+            map.put(c, i);
+            maxLength = Math.max(maxLength, i - l + 1);
         }
 
         return maxLength;
