327e755ebd78d4503075744c1e8ffcf49c6be2e8
Harish Puri <harishpuri.in@gmail.com>
2026-04-14 10:58:53 +0530

    ProductLessThanK

--- Sliding [ ]/ProductLessThanK.java
+++ Sliding [ ]/ProductLessThanK.java
@@ -1,5 +1,27 @@
 public class ProductLessThanK {
     public static void main(String[] args){
-        
+        int[] nums = { 10,9,10,4,3,8,3,3,6,2,10,10,9,3 };
+        int k = 19;
+        solution(nums, k);
+    }
+
+    static int solution(int[] nums, int k){
+        int left = 0;
+        int count = 0;
+        int currProduct = nums[0];
+
+        for(int i =1; i < nums.length; i++){
+            if(currProduct < k) count++;
+            if(nums[i] < k) count++;
+            currProduct = currProduct * nums[i];
+            if(currProduct >= k){
+                currProduct = currProduct / nums[left];
+                left++;
+            }else{
+                count++;
+            }
+        }
+
+        return count;
     }
 }
