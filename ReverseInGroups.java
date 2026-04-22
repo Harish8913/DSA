b0de90fafe65cd441c9be7222f0b81753d35f7f2
Harish Puri <harishpuri.in@gmail.com>
2026-04-22 08:31:04 +0530

    L1

--- ReverseInGroups.java
+++ ReverseInGroups.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class ReverseInGroups {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12 };
-        int k = 3;
+        int k = 3; 
 
         System.out.println(Arrays.toString(test(arr, k)));
     }
