00fa6f70794973e6368fe406cf3aeb76f49d73db
Harish Puri <harishpuri.in@gmail.com>
2026-05-01 15:56:19 +0530

    MinSubArray Solved

--- Sliding [ ]/MinSubArray.java
+++ Sliding [ ]/MinSubArray.java
@@ -1,7 +1,7 @@
 public class MinSubArray {
     public static void main(String[] args) {
-        int[] arr = { 1, 1, 2, 2, 2, 10, 10, 10, 20, 2, 2, 2, 4 };
-        int k = 4;
+        int[] arr = { 2,3,1,2,4,3 };
+        int k = 8;
 
         int value = test(arr, k);
         System.out.println(value);
@@ -9,24 +9,18 @@ public class MinSubArray {
 
     static int test(int[] nums, int target) {
         int l = 0;
-        int sum = 0;
+        int sum = 0;    
         int count = Integer.MAX_VALUE;
 
         for (int r = 0; r < nums.length; r++) {
-            if (sum >= target) {
-                while (l < r) {
-                    l++;
-                    sum -= nums[l];
-
-                    count = Math.min(count, r - l + 1);
-                }
-            } else {
-                sum += nums[r];
+            sum += nums[r];
+            while(sum >= target){
+                count = Math.min(count, r - l + 1);
+                sum -= nums[l];
+                l++;
             }
-
-            count = Math.min(count, r - l + 1);
         }
 
-        return count;
+        return count == Integer.MAX_VALUE ? 0 : count;
     }
 }
