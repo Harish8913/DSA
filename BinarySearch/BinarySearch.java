29ef6d6cc45735cd763fa2fc42855d6366289a50
Harish Puri <harishpuri.in@gmail.com>
2026-05-20 01:55:50 +0530

    binary search

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,25 @@
+package BinarySearch;
+
+public class BinarySearch {
+    public static void main(String[] args){
+        int[] nums = {1, 4, 6, 11, 34, 39, 94, 100};
+        int target = 4;
+        search(nums, target);
+    }
+
+    static void search(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = high / 2;
+
+        while(low <= high){
+            if(nums[mid] == target) break;
+
+            if(nums[mid] < target){
+                low = mid + 1;
+            }else if(nums[mid] > target){
+                high = mid - 1;
+            }
+        }
+    }
+}
