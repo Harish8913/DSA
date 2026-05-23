9d7f5d08b1fb5532d8f1575c8f1892956c364d96
Harish Puri <harishpuri.in@gmail.com>
2026-05-23 08:43:59 +0530

    SearchRotatedArray2

---
+++ BinarySearch/SearchInRotatedSorted2.java
@@ -0,0 +1,38 @@
+package BinarySearch;
+
+public class SearchInRotatedSorted2 {
+    public static void main(String[] args){
+        int[] nums = { 3, 1 };
+        int target = 0;
+        search(nums, target);
+    }
+    public static boolean search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target) return true;
+
+            if(nums[low] < nums[mid]){
+                if(target >= nums[low] && target < nums[mid]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }else if(nums[mid] < nums[high]){
+                if(target >= nums[mid] && target <= nums[high]){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }else{
+                if(nums[high] == target) return true;
+                else{high -= 1;}
+            }
+        }
+
+        return false;
+    }
+}
