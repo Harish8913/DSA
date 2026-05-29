55f5d2fb270483b9e53d380aa319e054f68e8ef9
Harish Puri <harishpuri.in@gmail.com>
2026-05-29 15:11:51 +0530

    LastOccurence

--- BinarySearch/LastOccurence.java
+++ BinarySearch/LastOccurence.java
@@ -1,25 +1,26 @@
 package BinarySearch;
 
-public class FirstOccurence {
+public class LastOccurence {
     public static void main(String[] args){
-        int[] nums = {1,1,2,2,2,3,3,3,4,5,6};
+        int nums[] = { 1,1,1,1,2,2,2,2,3,4,5 };
         int target = 2;
         int ans = search(nums, target);
         System.out.println(ans);
     }
 
-    public static int search(int[] nums, int target){
+    static int search(int[] nums, int target){
         int low = 0;
         int high = nums.length - 1;
 
         while(low <= high){
             int mid = low + (high - low) / 2;
-            if(nums[low] == target) return low;
+            if(nums[high] == target) return high;
 
             if(nums[mid] < target) low = mid + 1;
-            else if(nums[mid] >= target) high = mid;
-            else low += 1;
+            else if(nums[mid] > target) high = mid;
+            else high -= 1;
         }
+
         return -1;
     }
 }
