b88071c9172da8f44ef571a2f23660c82ead688c
Harish Puri <harishpuri.in@gmail.com>
2026-05-29 16:33:06 +0530

    occurenceOfElement

--- BinarySearch/FirstOccurence.java
+++ BinarySearch/FirstOccurence.java
@@ -3,7 +3,7 @@ package BinarySearch;
 public class FirstOccurence {
     public static void main(String[] args){
         int[] nums = {1,1,2,2,2,3,3,3,4,5,6};
-        int target = 2;
+        int target = 3;
         int ans = search(nums, target);
         System.out.println(ans);
     }
@@ -11,15 +11,20 @@ public class FirstOccurence {
     public static int search(int[] nums, int target){
         int low = 0;
         int high = nums.length - 1;
+        int ans = -1;
 
         while(low <= high){
             int mid = low + (high - low) / 2;
-            if(nums[low] == target) return low;
-
-            if(nums[mid] < target) low = mid + 1;
-            else if(nums[mid] >= target) high = mid;
-            else low += 1;
+            
+            if(nums[mid] == target){
+                ans = mid;
+                high = mid - 1;
+            }else if(nums[mid] < target){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
         }
-        return -1;
+        return ans;
     }
 }
--- BinarySearch/LastOccurence.java
+++ BinarySearch/LastOccurence.java
@@ -3,7 +3,7 @@ package BinarySearch;
 public class LastOccurence {
     public static void main(String[] args){
         int nums[] = { 1,1,1,1,2,2,2,2,3,4,5 };
-        int target = 2;
+        int target = 1;
         int ans = search(nums, target);
         System.out.println(ans);
     }
@@ -11,16 +11,20 @@ public class LastOccurence {
     static int search(int[] nums, int target){
         int low = 0;
         int high = nums.length - 1;
+        int ans = -1;
 
         while(low <= high){
             int mid = low + (high - low) / 2;
-            if(nums[high] == target) return high;
-
-            if(nums[mid] < target) low = mid + 1;
-            else if(nums[mid] > target) high = mid;
-            else high -= 1;
+            if(nums[mid] == target){
+                ans = mid;
+                low = mid + 1;
+            }else if(nums[mid] < target){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
         }
 
-        return -1;
+        return ans;
     }
 }
