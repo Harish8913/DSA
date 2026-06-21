94adbcf8b88407af20b8df5292b9191197f28bad
Harish Puri <harishpuri.in@gmail.com>
2026-06-21 20:37:20 +0530

    SlidingW

--- Sliding [ ]/SlidingWTemplate.java
+++ Sliding [ ]/SlidingWTemplate.java
@@ -1,3 +1,10 @@
 public class SlidingWTemplate {
-    
+    public static void main(String[] args){
+        int[] nums = {  };
+
+    }
+
+    public static int slide(int[] nums, int k){
+        return 0;
+    }
 }
